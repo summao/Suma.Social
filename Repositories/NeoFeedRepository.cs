09765effd8e611056c4f8f956cb35b1a4169c4af
maoindy <maoindy@odds.team>
2021-02-23 20:59:45 +0700

    add feed

--- Repositories/NeoFeedRepository.cs
+++ Repositories/NeoFeedRepository.cs
@@ -1,7 +1,23 @@
+using System.Threading.Tasks;
+using Neo4jClient;
+using Suma.Social.Entities;
+
 namespace Suma.Social.Repositories
 {
     public class NeoFeedRepository
     {
+        private readonly IGraphClient _graphClient;
 
+        public NeoFeedRepository(IGraphClient graphClient)
+        {
+            _graphClient = graphClient;
+        }
+
+        public async Task<Feed> GetAsync()
+        {
+            var feeds = await _graphClient.Cypher.Match("(p:Person{Id:'1'})-[:Post]->(f:Feed)")
+                .Return()
+        }
+    
     }
 }
