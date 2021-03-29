fdbf933d0d4bcc96ee149a542de14ccbc0df865b
maoindy <maoindy@odds.team>
2021-03-29 11:35:21 +0700

    refactor move RunAsync to base class

---
+++ Repositories/BaseNeoRepository.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Neo4j.Driver;
+
+namespace Suma.Social.Repositories
+{
+    public class BaseNeoRepository
+    {
+        private readonly IDriver _driver;
+
+        public BaseNeoRepository(IDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public async Task<T> ExecuteSingleAsync<T>(string query,Dictionary<string, object> parameters, Func<IRecord,T> func)
+        {
+            var session = _driver.AsyncSession();
+            try
+            {
+                var cursor = await session.RunAsync(query, new { a = parameters });
+
+                return await cursor.SingleAsync<T>(func);
+            }
+            finally
+            {
+                await session.CloseAsync();
+            }
+        }
+
+        public async Task<List<T>> ExecuteListAsync<T>(string query,Dictionary<string, object> parameters, Func<IRecord,T> func)
+        {
+            var session = _driver.AsyncSession();
+            try
+            {
+                var cursor = await session.RunAsync(query, new { a = parameters });
+
+                return await cursor.ToListAsync<T>(func);
+            }
+            finally
+            {
+                await session.CloseAsync();
+            }
+        }
+
+    }
+}
+
--- Repositories/NeoCommentRepository.cs
+++ Repositories/NeoCommentRepository.cs
@@ -13,20 +13,16 @@ namespace Suma.Social.Repositories
         Task<List<Comment>> GetManyAsync(string postId);
     }
 
-    public class NeoCommentRepository : INeoCommentRepository
+    public class NeoCommentRepository : BaseNeoRepository, INeoCommentRepository
     {
-        private readonly IDriver _driver;
 
-        public NeoCommentRepository(IDriver driver)
+        public NeoCommentRepository(IDriver driver) : base(driver)
         {
-            _driver = driver;
+
         }
 
         public async Task<CreateCommentResponse> InsertAsync(Comment comment, int commentorId, string replyToPostId)
         {
-            var session = _driver.AsyncSession();
-            var parameters = comment.AsDictionary();
-            parameters.Add("commentorId", commentorId);
 
             var query = @"MATCH (f:Post {id: $a.replyToPostId})
                 MATCH (p:Person {id: $a.commentorId})
@@ -41,32 +37,24 @@ namespace Suma.Social.Repositories
                     f.id as ReplyToPostId,
                     p.id as CommentorId";
 
+            var parameters = comment.AsDictionary();
+            parameters.Add("commentorId", commentorId);
             parameters.Add("replyToPostId", replyToPostId);
 
-            try
-            {
-                var cursor = await session.RunAsync(query, new { a = parameters });
-
-                return await cursor.SingleAsync(r =>
-                    new CreateCommentResponse
-                    {
-                        CommentId = r["CommentId"].As<string>(),
-                        Text = r["Text"].As<string>(),
-                        Created = r["Created"].As<DateTimeOffset>(),
-                        ReplyToPostId = r["ReplyToPostId"].As<string>(),
-                        CommentorId = r["CommentorId"].As<int>(),
-                    }
-                );
-            }
-            finally
-            {
-                await session.CloseAsync();
-            }
+            return await ExecuteSingleAsync(query, parameters, r =>
+                new CreateCommentResponse
+                {
+                    CommentId = r["CommentId"].As<string>(),
+                    Text = r["Text"].As<string>(),
+                    Created = r["Created"].As<DateTimeOffset>(),
+                    ReplyToPostId = r["ReplyToPostId"].As<string>(),
+                    CommentorId = r["CommentorId"].As<int>(),
+                }
+            );
         }
 
         public async Task<List<Comment>> GetManyAsync(string postId)
         {
-            var session = _driver.AsyncSession();
             var parameters = new Dictionary<string, object> {
                 {"postId", postId}
             };
@@ -76,23 +64,14 @@ namespace Suma.Social.Repositories
                     c.text as Text,
                     c.created as Created";
 
-            try
-            {
-                var cursor = await session.RunAsync(query, new { a = parameters });
-
-                return await cursor.ToListAsync(r =>
-                    new Comment
-                    {
-                        Id = r["Id"].As<string>(),
-                        Text = r["Text"].As<string>(),
-                        Created = r["Created"].As<DateTimeOffset>(),
-                    }
-                );
-            }
-            finally
-            {
-                await session.CloseAsync();
-            }
+            return await ExecuteListAsync(query, parameters,r =>
+                new Comment
+                {
+                    Id = r["Id"].As<string>(),
+                    Text = r["Text"].As<string>(),
+                    Created = r["Created"].As<DateTimeOffset>(),
+                }
+            );
         }
     }
 }
